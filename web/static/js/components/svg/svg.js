3150c03f3607fba83113f3c1bef466fcd20a94ab
Toby Privett <toby@snaplab.co.uk>
2016-07-12 17:05:53 +0100

    Map translates with the zoom
    - it's rough but I envisage users dragging also

--- web/static/js/components/svg/svg.js
+++ web/static/js/components/svg/svg.js
@@ -5,18 +5,32 @@ export default class Svg extends React.Component {
   constructor() {
     super()
     this.state = {
-      scale: 1
+      scale: 1,
+      translate: 0
     }
     this.handleZoomIn = this.handleZoomIn.bind(this)
     this.handleZoomOut = this.handleZoomOut.bind(this)
+    this.handleResetZoom = this.handleResetZoom.bind(this)
   }
 
   handleZoomIn() {
-    this.setState({scale: this.state.scale * 1.2})
+    this.setState({
+      scale: this.state.scale * 1.2,
+      translate: this.state.translate - 40
+    })
   }
 
   handleZoomOut() {
-    this.setState({scale: this.state.scale / 1.2})
+    this.setState({
+      scale: this.state.scale / 1.2,
+      translate: this.state.translate + 40
+    })
+  }
+
+  handleResetZoom() {
+    this.setState({
+      scale: 1, translate: 0
+    })
   }
 
   renderPolylines(surveys) {
@@ -32,7 +46,7 @@ export default class Svg extends React.Component {
   render() {
     const cave = this.props.cave
     const surveys = cave.surveys || []
-    console.log(this.state.scale)
+    console.log(this.state.translate)
     return (
       <div>
         <div className="btn-group">
@@ -42,12 +56,18 @@ export default class Svg extends React.Component {
           <button className="btn btn-default" onClick={this.handleZoomOut}>
             <span className="glyphicon glyphicon-minus" />
           </button>
+          <button className="btn btn-default" onClick={this.handleResetZoom}>
+            Reset
+          </button>
         </div>
 
         <div id="map-container">
           <svg key={cave.id} width="100%" viewBox="0 0 500 500">
             <g
-              transform={`scale(${this.state.scale})`}>
+              transform={`
+                translate(${this.state.translate} ${this.state.translate})
+                scale(${this.state.scale})
+              `}>
               {this.renderPolylines(surveys)}
             </g>
           </svg>
