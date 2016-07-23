cbf1a5f5f692da81cd1c9cb99c7e106255180bb1
Toby Privett <toby@snaplab.co.uk>
2016-07-23 11:23:57 +0100

    Update the mix file

--- mix.exs
+++ mix.exs
@@ -9,6 +9,7 @@ defmodule Cavemapper.Mixfile do
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     aliases: aliases,
      deps: deps]
   end
 
@@ -17,7 +18,9 @@ defmodule Cavemapper.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {Cavemapper, []},
-     applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext]]
+     applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext,
+                    :phoenix_ecto, :postgrex, :earmark, :ex_doc, :chronos,
+                    :ex_machina, :geo]]
   end
 
   # Specifies which paths to compile per environment.
@@ -29,9 +32,31 @@ defmodule Cavemapper.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [{:phoenix, "~> 1.1.4"},
+     {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.4"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
+     {:phoenix_ecto, "~> 3.0"},
      {:gettext, "~> 0.9"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"}],
+     {:earmark, "~> 0.1"},
+     {:ex_doc, "~> 0.11"},
+     {:chronos, "~> 1.5.1"},
+     {:ex_machina, "~> 0.6.1"},
+     {:geo, "~> 1.0"},
+     {:credo, "~> 0.4", only: [:dev, :test]},
+     {:basic_auth, ">= 0.0.1"},
+     {:corsica, "~> 0.5.0"}
+    ]
+  end
+
+  # Aliases are shortcut or tasks specific to the current project.
+  # For example, to create, migrate and run the seeds file at once:
+  #
+  #     $ mix ecto.setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+     "ecto.reset": ["ecto.drop", "ecto.setup"]]
   end
 end
